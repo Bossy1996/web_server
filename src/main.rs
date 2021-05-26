b3f5902422fefebe8b185c2ce4b1986a50f879d3
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-26 18:39:01 +0200

    Returning real html

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
+use std::fs;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
@@ -17,7 +19,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
