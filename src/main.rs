efed4db21336643c4238b592a97de90718e82453
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-26 18:53:47 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r|n";
+    let get = b"GET / HTTP/1.1\r\n";
 
     if buffer.starts_with(get) {
 
