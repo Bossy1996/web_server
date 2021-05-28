83e4d61d4e1ebbaa5973e95943154d5221d67678
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-28 17:30:56 +0200

    ThreadPool new and execute methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
-    pub fn new(x: u32) {}
+    /// Create a new Threadpool.
+    /// 
+    /// The size is the number of threads in the pool.
+    /// 
+    /// # Panics
+    /// 
+    /// The `new`function will panic if the size is 0.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        ThreadPool
+    }
+    pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {
+
+    }
 }
