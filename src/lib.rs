7a9f38eaac2e2c3325a36b113fa54290a8fbf757
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-29 22:27:00 +0200

    Work struct for send code from the pool to thread

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,9 @@ impl ThreadPool {
 
         let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
+        for id in 0..size {
             // create some threads and store them in the vector
+            workers.push(Worker::new(id));
         }
 
         ThreadPool { workers }
